b42fa0119ae61335f83ae29057d16a124516ce9b
Azzed Khursheed <azzedkhursheed47@gmail.com>
2025-04-30 01:02:34 -0500

    Fixed it

--- YA.App/App.xaml.cs
+++ YA.App/App.xaml.cs
@@ -9,7 +9,17 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var window = new Window(new AppShell());
+            // Ensure Shell is initialized before calling navigation
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                if (Shell.Current != null)
+                {
+                    await Shell.Current.GoToAsync("//MainPage");
+                }
+            });
+
+            return window;
         }
     }
 }
--- YA.App/Pages/MainPage.xaml.cs
+++ YA.App/Pages/MainPage.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using YA.App.Models;
 using YA.App.PageModels;
+using YA.App.ViewModels;
 
 namespace YA.App.Pages
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        public MainPage(MainPageViewModel viewModel)
         {
             InitializeComponent();
+
+            BindingContext = viewModel;
         }
     }
 }
